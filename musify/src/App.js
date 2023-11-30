f01a397d4669927744d1d8c763ad3741f3fa74c5
frankmkpark <frankmkpark@gmail.com>
2023-11-30 10:55:23 -0500

    Main page done w/o Dashboard button

--- musify/src/App.js
+++ musify/src/App.js
@@ -1,29 +1,14 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './navbar';
+import Home from './home';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 
--- musify/src/home/index.js
+++ musify/src/home/index.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 function Home() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchType, setSearchType] = useState('Songs');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+        console.log('Search Term:', searchTerm);
+        console.log('Search Type:', searchType);
+        // Add search logic here
+    };
+
+    const handleSearchTypeChange = (type) => {
+        setSearchType(type);
+    };
+
     return (
         <div className="App">
-            <header className="App-header">
-                {/* Navigation Bar */}
-                <nav className="App-nav">
-                    <h1>Musify</h1>
-                    <div>
-                        <a href="#home">Home</a>
-                        <a href="#signin">Sign-in/up</a>
+                <div className="playlist-section">
+                    <br /><br />
+                    <h2 className="playlist-heading">Make your Playlist</h2>
+                    <br /><br />
+                    <div className="search-types">
+                        {['Songs', 'Albums', 'Artists', 'Playlists'].map((type) => (
+                            <button
+                                key={type}
+                                className={`search-type-button ${searchType === type ? 'active' : ''}`}
+                                onClick={() => handleSearchTypeChange(type)}
+                            >
+                                {type}
+                            </button>
+                        ))}
+                        <br /><br />
                     </div>
-                </nav>
-
-                {/* Main Content */}
-                {/* <img src={logo} className="App-logo" alt="logo" /> */}
-                <p>
-                    Edit
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+                    <div className="search-container">
+                        <form onSubmit={handleSearchSubmit} className="form-inline">
+                            <input
+                            type="text"
+                            className="form-control search-bar mb-2 mr-sm-2"
+                            placeholder="Search for songs, artists, albums, playlists"
+                            value={searchTerm}
+                            onChange={handleSearchChange}/>
+                            <button type="submit" className="btn btn-primary mb-2">Search</button>
+                        </form>
+                    </div>
+                </div>
         </div>
     );
 }
